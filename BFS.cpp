99f1a265a3d6a658b2ef2f6f9074e0c1abd69ac5
Ben Holland <ben.holland.core@gmail.com>
2017-03-10 12:50:16 -0800

    Completed BFS Search

--- BFS.cpp
+++ BFS.cpp
@@ -1,18 +1,14 @@
-/*
- * Title: Homework 5 Breadth First Search Demo.
- * Author: Benjamin Holland.
- * ID: 4338
- * Creation Date:  3/8/2017;
- */
+
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <memory>
 #include <string>
 #include <exception>
-#include <set>
-#include <queue>
+#include <queue> //Don't wnat to implement my own queue
 #include <algorithm>
+#include <cstring> //Need memset
 using namespace std;
 
 class Graph{
@@ -38,48 +34,45 @@ public:
 
 	bool getEdge(unsigned int from,unsigned int to) const;
 
-	set<unsigned int> getAdjacentVerticies(unsigned int currentVertex) const;
+	string getBFTDisplay(unsigned int startingNode){
+		if(startingNode>getCount()){
+			throw exception();
+		}else{
+			queue<unsigned int> nodesToVisit;
+			bool* visited=new bool[getCount()];
+			memset(visited,0,sizeof(bool)*getCount());
+			nodesToVisit.push(startingNode);
+			stringstream output;
+			while(!nodesToVisit.empty()){
+				unsigned int currentIdx=nodesToVisit.front();
+				nodesToVisit.pop();
+				if(!visited[currentIdx]){
+					output<<currentIdx<<"->";
+					visited[currentIdx]=true;
+					for(unsigned int toIdx=0;toIdx<getCount();toIdx++){
+						if(getEdge(currentIdx,toIdx)){
+							nodesToVisit.push(toIdx);
+						}
+					}
+				}
+			}
+
+			delete[] visited;
+			string retval=output.str();
+			return retval.substr(0,retval.length()-2); //O(1) truncation.
+		}
+
+	}
 
 
 };
 
-void testGraphConstruction(){
-	//File presented in design document. Faked here to be a stream so we don't have to do file open checks and handling.
-	stringstream fakeFile("3\n0 1 0\n1 0 1\n0 1 0");
-	fakeFile.seekp(0);
-	Graph g(fakeFile);
-	cout<<g.toString()<<endl;
-	for(int idx=0;idx<g.getCount();idx++){
-		cout<<"Vertecies Adjacent to "<<idx<<": ";
-		set<unsigned int> adj=g.getAdjacentVerticies(idx);
-		for_each(begin(adj),end(adj),[](unsigned int adjItem){
-			cout<<adjItem<<" ";
-		});
-		cout<<endl;
-
-	}
-}
-
-
-void testGraphConstruction2(){
-	//File presented in design document. Faked here to be a stream so we don't have to do file open checks and handling.
-	ifstream dataStream("./sample_t2.txt");
-	Graph g(dataStream);
-	cout<<g.toString()<<endl;
-	for(int idx=0;idx<g.getCount();idx++){
-		cout<<"Vertecies Adjacent to "<<idx<<": ";
-		set<unsigned int> adj=g.getAdjacentVerticies(idx);
-		for_each(begin(adj),end(adj),[](unsigned int adjItem){
-			cout<<adjItem<<" ";
-		});
-		cout<<endl;
-
-	}
-}
 
 
 int main(int argc,char* argv[]){
-	testGraphConstruction2();
+	ifstream data("./sample_t2.txt");
+	Graph g(data);
+	cout<<g.getBFTDisplay(0)<<endl;
 }
 
 Graph::Graph(istream& dataStream){
@@ -88,10 +81,10 @@ Graph::Graph(istream& dataStream){
 	//create a unique_pointer.
 	adjacency=make_unique<bool[]>(count*count);
 	unsigned int state;
-	for(int fromIdx=0;fromIdx<count;fromIdx++){
-		for(int toIdx=0;toIdx<count;toIdx++){
+	for(unsigned int fromIdx=0;fromIdx<count;fromIdx++){
+		for(unsigned int toIdx=0;toIdx<count;toIdx++){
 			dataStream>>state;
-			cout<<"Edge from  "<<fromIdx<<" to "<<toIdx<<" "<<state<<endl;
+			//cout<<"Edge from  "<<fromIdx<<" to "<<toIdx<<" "<<state<<endl;
 			if(state==1){
 				setEdge(fromIdx,toIdx,true);
 			}
@@ -99,10 +92,10 @@ Graph::Graph(istream& dataStream){
 	}
 }
 
-
 unsigned int Graph::getCount() const{
 	return count;
 }
+
 string Graph::toString() const{
 	stringstream out;
 	for(int row=0;row<count;row++){
@@ -132,14 +125,4 @@ void Graph::setEdge(unsigned int from,unsigned int to,bool connected){
 			throw exception();
 		}
 		adjacency[from*count+to]=connected;
-}
-
-set<unsigned int> Graph::getAdjacentVerticies(unsigned int current) const{
-	set<unsigned int> ret;
-	for(int toIdx=0;toIdx<count;toIdx++){
-		if(getEdge(current,toIdx)){
-			ret.emplace(toIdx);
-		}
-	}
-	return ret;
 }
